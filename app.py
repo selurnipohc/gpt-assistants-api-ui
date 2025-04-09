8026e34c9c7b8ce8d8e6242ab4bf8fd2928b107b
selurnipohc <97242610+selurnipohc@users.noreply.github.com>
2025-04-09 10:44:06 -0500

    Update app.py

--- app.py
+++ app.py
@@ -67,6 +67,7 @@ enabled_file_upload_message = False
 client = openai.OpenAI(api_key=openai_api_key)
 
 st.set_page_config(page_title = "Diversions Bot",page_icon="./favicon.ico",layout="wide")
+st.session_state["lastClicked"] = None
 
 class EventHandler(AssistantEventHandler):
     @override
